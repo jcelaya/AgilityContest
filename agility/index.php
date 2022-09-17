63772591b7f59f17e9b3a0086f834ed547554194
Javier Celaya <jcelaya@gmail.com>
2022-09-17 15:23:21 +0200

    Take port into account in redirection

--- agility/index.php
+++ agility/index.php
@@ -29,6 +29,10 @@
         // from: https://stackoverflow.com/questions/15110355/how-to-safely-get-full-url-of-parent-directory-of-current-php-page
         $url  = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
         $url .= $_SERVER['SERVER_NAME'];
+        if ((isset($_SERVER['HTTPS']) && $_SERVER['SERVER_PORT'] !== "443")
+            || (!isset($_SERVER['HTTPS']) && $_SERVER['SERVER_PORT'] !== "80")) {
+            $url .= ":" . $_SERVER['SERVER_PORT'];
+        }
         $path= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $res=str_replace("index.php","public/index.php",$path,$count);
         if ($count===0) $url .= $res ."public/index.php";
