0a0289b76b0fe97bbd9d7a240036893bc3385c4f
Javier Celaya <jcelaya@gmail.com>
2023-01-01 11:59:56 +0100

    Grant user permissions in localhost only for standalone installations

---
+++ extras/users_dockerized.sql
@@ -0,0 +1,12 @@
+# Usuario 'admin' Dios (yomismo :-)
+GRANT USAGE ON `agility`.* TO 'agility_admin' IDENTIFIED BY PASSWORD '*08B004F5A4F0C12BC39ACA150D08073AC58298C7';
+GRANT ALL PRIVILEGES ON `agility`.* TO 'agility_admin' WITH GRANT OPTION;
+# Usuario 'operator' anyadir/borrar/modificar datos (sala de control)
+GRANT USAGE ON `agility`.* TO 'agility_operator' IDENTIFIED BY PASSWORD '*133E83223499F08EAABCB24B71D08169FA241A9B';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES, LOCK TABLES, TRIGGER, EXECUTE, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, EVENT ON `agility`.* TO 'agility_operator';
+# Usuario 'user' insertar datos (ayudante de pista)
+GRANT USAGE ON `agility`.* TO 'agility_user' IDENTIFIED BY PASSWORD '*FA79D4F71FE9C4BEF173D2D0D8535A9A38253676';
+GRANT SELECT, INSERT, UPDATE, CREATE TEMPORARY TABLES, EXECUTE, SHOW VIEW ON `agility`.* TO 'agility_user';
+# Usuario 'guest' consultar datos (invitados)
+GRANT USAGE ON `agility`.* TO 'agility_guest' IDENTIFIED BY PASSWORD '*A93683F47D32463876747B97C62B8BF4CDD14E7C';
+GRANT SELECT, EXECUTE, SHOW VIEW ON `agility`.* TO 'agility_guest';
